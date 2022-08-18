d02de1210844067f6d4fcb7ec2e6f995b720207d
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-08-18 15:36:19 -0300

    make exportEvents receive ProcessedPluginEvent[]

--- src/types.ts
+++ src/types.ts
@@ -21,7 +21,7 @@ export interface Plugin<Input extends PluginInput = {}> {
     /** DEPRECATED: Receive a batch of events and return it in its processed form. You can discard events by not including them in the returned array. You can also append additional events to the returned array. */
     processEventBatch?: (eventBatch: PluginEvent[], meta: Meta<Input>) => PluginEvent[] | Promise<PluginEvent[]>
     /** Receive a single non-snapshot event.  */
-    exportEvents?: (events: PluginEvent[], meta: Meta<Input>) => void | Promise<void>
+    exportEvents?: (events: ProcessedPluginEvent[], meta: Meta<Input>) => void | Promise<void>
     /** Receive a single processed event. */
     onEvent?: (event: ProcessedPluginEvent, meta: Meta<Input>) => void | Promise<void>
     /** Receive a single snapshot (session recording) event. */
